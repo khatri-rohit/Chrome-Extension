4ea46ad70dcfb78d65f1c6042df131af5db83a17
Rohit <rohitkhatri111112@gmail.com>
2024-12-01 16:51:56 +0530

    Script Connection and response ✔️

--- chrome_tutorials/videohas/content.js
+++ chrome_tutorials/videohas/content.js
@@ -1,7 +1,18 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        if (request.greeting === "hello") {
-            sendResponse({ farewell: "goodbye" });
+        if (request.action === "getTitle") {
+            const h1 = document.body.querySelector("h1");
+            h1.style.color = "royalblue"
+
+            const title = document.title;
+            sendResponse({ title: title, head1: h1 });
+        } else if (request.videoElement === "getVideo") {
+            const video = document.body.querySelector("video");
+
+            sendResponse({
+                sender: sender,
+                video: video.className,
+            })
         }
     }
 );
--- chrome_tutorials/videohas/popup.js
+++ chrome_tutorials/videohas/popup.js
@@ -4,8 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
         const [tab] = await chrome.tabs.query(
             { active: true, currentWindow: true }
         );
-        const response = await chrome.tabs.sendMessage(tab.id, { greeting: "hello" });
+        const response = await chrome.tabs.sendMessage(tab.id,
+            // { action: "getTitle" }
+            { videoElement: "getVideo" }
+        );
         // do something with response here, not outside the function
         console.log(response);
+        console.log(response.video);
     });
 });
